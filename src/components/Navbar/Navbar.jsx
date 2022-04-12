c2f0ca6174b3144eab8472088f4760b9883a667a
RuhaimHakeem <ruhaim860@gmail.com>
2022-04-12 11:35:55 +0530

    Added Work Section

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,12 +11,14 @@ const Navbar = () => {
         <img src={images.logo} alt="" />
       </div>
       <ul className="app__navbar-links">
-        {["home", "about", " work", "skills", "contact"].map((item) => (
-          <li className="app__flex p-text" key={`link-${item}`}>
-            <div />
-            <a href={`#${item}`}>{item}</a>
-          </li>
-        ))}
+        {["home", "about", " work", "skills", "testimonials", "contact"].map(
+          (item) => (
+            <li className="app__flex p-text" key={`link-${item}`}>
+              <div />
+              <a href={`#${item}`}>{item}</a>
+            </li>
+          )
+        )}
       </ul>
 
       <div className="app__navbar-menu">
@@ -28,7 +30,14 @@ const Navbar = () => {
           >
             <HiX onClick={() => setToggle(false)} />
             <ul>
-              {["home", "about", " work", "skills", "contact"].map((item) => (
+              {[
+                "home",
+                "about",
+                " work",
+                "skills",
+                "testimonials",
+                "contact",
+              ].map((item) => (
                 <li key={item}>
                   <a href={`#${item}`} onClick={() => setToggle(false)}>
                     {item}
--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,38 +1,17 @@
 import { useState, useEffect } from "react";
 import "./About.scss";
 import { motion } from "framer-motion";
-import { images } from "../../constants";
+import { urlFor, client } from "../../client";
+import { AppWrap } from "../../wrapper";
 
-const abouts = [
-  {
-    title: "Web Development",
-    description: "I am a good web developer",
-    imgUrl: images.about01,
-  },
-
-  {
-    title: "Frontend Development",
-    description: "I am a good web developer",
-    imgUrl: images.about02,
-  },
-
-  {
-    title: "UI/UX",
-    description: "I am a good web developer",
-    imgUrl: images.about03,
-  },
-
-  {
-    title: "MERN Stack",
-    description: "I am a good web developer",
-    imgUrl: images.about04,
-  },
-];
 const About = () => {
   const [abouts, setAbouts] = useState([]);
 
   useEffect(() => {
     const query = '*[_type == "abouts"]';
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
   }, []);
 
   return (
@@ -52,7 +31,7 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>
               {about.title}
             </h2>
@@ -67,4 +46,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default AppWrap(About, "about");
